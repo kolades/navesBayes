2fef102bee0c93cf835206dff605fdf239ab525d
kolades <117230554+kolades@users.noreply.github.com>
2022-11-02 22:26:34 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -64,5 +64,5 @@ SepalWidthCm = st.number_input('Digite a largura do caule')
 PetalLengthCm = st.number_input('Digite o comprimento da petala')
 PetalWidthCm = st.number_input('Digite a largura da petala')
 if st.button('Clique aqui'):
-  resultado = floresta.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
+  resultado = model.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
   st.write('Resultado:',resultado)
